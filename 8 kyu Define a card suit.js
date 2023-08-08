0fae4afcc1e9fe3275471962968355fa82477657
Sergey Chepelev <96368552+kabawonga@users.noreply.github.com>
2023-08-08 11:53:57 +0600

    Add files via upload

---
+++ 8 kyu Define a card suit.js
@@ -0,0 +1,11 @@
+function defineSuit(card) {
+  if (card[card.length - 1] === "♣") {
+    return "clubs";
+  } else if (card[card.length - 1] === "♦") {
+    return "diamonds";
+  } else if (card[card.length - 1] === "♥") {
+    return "hearts";
+  } else if (card[card.length - 1] === "♠") {
+    return "spades";
+  }
+}
