a525b68a66fcb73e686446292477c1d10df831f1
Sergey Chepelev <96368552+kabawonga@users.noreply.github.com>
2023-08-13 15:11:29 +0600

    Add files via upload

---
+++ 8 kyu Alan Partridge II - Apple Turnover.js
@@ -0,0 +1,7 @@
+function apple(x) {
+  if (Number(x) * Number(x) > 1000) {
+    return "It's hotter than the sun!!";
+  } else {
+    return "Help yourself to a honeycomb Yorkie for the glovebox.";
+  }
+}
