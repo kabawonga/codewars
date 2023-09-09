13b892fd67abbb001a73ca89bc6d9f2d80ed7a5c
Sergey Chepelev <96368552+kabawonga@users.noreply.github.com>
2023-09-09 10:57:02 +0600

    Add files via upload

---
+++ 7 kyu Honey to the Bee.js
@@ -0,0 +1,25 @@
+class Bee {
+  constructor(capacity, hive) {
+    this.capacity = capacity;
+    this.hive = hive;
+  }
+}
+
+class Hive {
+  constructor() {
+    this.pollen = 100;
+  }
+}
+
+//TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function (pollen) {
+  this.pollen += pollen;
+};
+
+Hive.prototype.getPollen = function () {
+  return this.pollen;
+};
+
+Bee.prototype.unloadPollen = function () {
+  this.hive.addPollen(this.capacity);
+};
