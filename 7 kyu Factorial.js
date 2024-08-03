e24231aa3b2ab50af15caa5e3e234e6a1e4d4ff1
Sergey Chepelev <kabawonga@gmail.com>
2024-08-03 21:13:52 +0500

    Add files via upload

--- 7 kyu Factorial.js
+++ 7 kyu Factorial.js
@@ -1,16 +1,1 @@
-const factorial = (n) => {
-	if (n < 0 || n > 12) {
-		throw new RangeError('Входное значение должно быть от 0 до 12')
-	}
-
-	if (n === 0) {
-		return 1
-	}
-
-	let result = 1
-	for (let i = 1; i <= n; i++) {
-		result *= i
-	}
-
-	return result
-}
+const factorial = (n) => (n <= 1 ? 1 : n * factorial(n - 1))
