2b6759be34ccd25aab3494c47f9b1ee4dd536b7b
Sergey Chepelev <kabawonga@gmail.com>
2024-09-29 21:44:36 +0500

    Add files via upload

---
+++ 7 kyu Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer.js
@@ -0,0 +1,6 @@
+const getFirstPython = (list) => {
+	const dev = list.find((el) => el.language === 'Python')
+	return dev
+		? `${dev.firstName}, ${dev.country}`
+		: 'There will be no Python developers'
+}
