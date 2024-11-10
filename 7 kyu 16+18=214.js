f06508f249f3631da2141b1f832845ee8ab653f8
Sergey Chepelev <kabawonga@gmail.com>
2024-11-10 23:55:33 +0500

    Update 7 kyu 16+18=214.js

--- 7 kyu 16+18=214.js
+++ 7 kyu 16+18=214.js
@@ -1,16 +1,13 @@
 const add = (num1, num2) => {
 	let [longArr, shortArr] = [String(num1), String(num2)]
-
 	if (shortArr.length > longArr.length) {
 		;[longArr, shortArr] = [shortArr, longArr]
 	}
-
 	const head = longArr.slice(0, longArr.length - shortArr.length)
 	const tail = longArr
 		.slice(-shortArr.length)
 		.split('')
 		.map((digit, index) => Number(digit) + Number(shortArr[index]))
 		.join('')
-
 	return Number(head + tail)
 }
